573578d479c6c5b88833a56259fe415676e33775
miimeunm <rhranfe@gmail.com>
2023-06-08 17:26:51 +0900

    CUD 추가

--- board/urls.py
+++ board/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('list/', views.list, name="list"),
     path('detail/<int:id>/', views.detail, name="detail"),
+    path('add/', views.addForm, name="addForm"),
+    path('edit/<int:id>', views.editForm, name="editForm"),
+    path('delete/<int:id>', views.deleteForm, name="deleteForm"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Board
 
 def index(request) :
@@ -15,9 +16,55 @@ def list(request) :
 
 def detail(request, id) :
     list = Board.objects.get(id=id)
+    list.incrementReadCount()
     
     context = {
         'list' : list,
     }
     
     return render(request, 'board/detail.html', context)
+
+def addForm(request) :
+    if request.method == 'POST' :
+        title = request.POST['title']
+        writer = request.POST['writer']
+        content = request.POST['content']
+        
+        Board.objects.create(title=title, writer=writer, content=content)
+        
+        return HttpResponseRedirect(reverse('board:list'))
+
+    else :
+        return render(request, 'board/addForm.html')
+
+def editForm(request, id) :
+       list = Board.objects.get(id=id)
+       context = {
+           'list' : list,
+       }
+       
+       if request.method == 'POST' :
+            title = request.POST['title']
+            writer = request.POST['writer']
+            content = request.POST['content']
+            
+            Board.objects.filter(id=id).update(title=title, writer=writer, content=content)
+            return HttpResponseRedirect(reverse('board:detail', args=(list.id,)))
+        
+       else :
+           return render(request, 'board/editForm.html', context)
+       
+def deleteForm(request, id) :
+    list = Board.objects.get(id=id)
+    context = {
+        'list' : list,
+    }
+    
+    if request.method == 'POST' :
+        Board.objects.filter(id=id).delete()
+        return HttpResponseRedirect(reverse('board:list'))
+    
+    else :
+        return render(request, 'board/deleteForm.html', context)
+    
+    
